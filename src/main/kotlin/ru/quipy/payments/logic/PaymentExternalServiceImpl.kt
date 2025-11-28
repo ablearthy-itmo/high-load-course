a1f1d55f49e635f9af812d2c26dd40a25fdd9b72
isagila <isagila@mail.ru>
2025-11-28 19:43:22 +0300

    500ms

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalServiceImpl.kt
@@ -76,7 +76,7 @@ class PaymentExternalSystemAdapterImpl(
 
     private val incomingLock = ReentrantLock()
 
-    private val rateLimiter = SlidingWindowRateLimiter(rateLimitPerSec.toLong() / 5, Duration.ofMillis(200L))
+    private val rateLimiter = SlidingWindowRateLimiter(rateLimitPerSec.toLong() / 2, Duration.ofMillis(500L))
     // private val rateLimiter = FixedWindowRateLimiter(rateLimitPerSec, 1000, TimeUnit.MILLISECONDS)
     private val ongoingWindow = Semaphore(parallelRequests)
 
