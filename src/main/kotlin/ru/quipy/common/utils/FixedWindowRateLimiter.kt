5086ffe90867aec93c629bb7712fad2218f76053
isagila <isagila@mail.ru>
2025-11-28 19:12:21 +0300

    fixed rl

--- src/main/kotlin/ru/quipy/common/utils/FixedWindowRateLimiter.kt
+++ src/main/kotlin/ru/quipy/common/utils/FixedWindowRateLimiter.kt
@@ -53,6 +53,11 @@ class FixedWindowRateLimiter(
     override fun tick() = semaphore.tryAcquire()
 
     fun tickBlocking() = semaphore.acquire()
+
+    suspend fun tickCoro() {
+        while (!tick()) { delay(10L) }
+    }
+
 }
 
 class SlowStartRateLimiter(
